91fb27afa74771c074ffaffb774857a88f920cbf
kshitij <kshitijkolhe91999@gmail.com>
2023-10-11 18:00:26 +0100

    initial commit

---
+++ src/main/java/com/kshitij/clinicaltrial/ClinicalTrialApplication.java
@@ -0,0 +1,13 @@
+package com.kshitij.clinicaltrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalTrialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicalTrialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kshitij/clinicaltrial/ClinicalTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kshitij.clinicaltrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalTrialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
