149203b53289b31558e71d4166c4d0658dc096f1
kshitij <kshitijkolhe91999@gmail.com>
2023-10-12 20:22:23 +0100

    Fetching patients by Id.

--- src/main/java/com/kshitij/clinicaltrial/controller/InquiryController.java
+++ src/main/java/com/kshitij/clinicaltrial/controller/InquiryController.java
@@ -4,11 +4,14 @@ package com.kshitij.clinicaltrial.controller;
 
 import com.kshitij.clinicaltrial.model.Patient;
 import com.kshitij.clinicaltrial.service.PatientRegistry;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class InquiryController {
@@ -24,6 +27,20 @@ public class InquiryController {
     public List<Patient> getAllEnrolledPatients() {
 
         return patientRegistry.getAllPatients();
+    }
+
+    @GetMapping(value = "/patient/{id}")
+    public Patient getPatient(@PathVariable("id") final int patientId, final HttpServletResponse httpServletResponse) {
+
+        final Optional<Patient> patient = patientRegistry.getPatient(patientId);
+
+        if (patient.isPresent()) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+            return patient.get();
+        }
+
+        httpServletResponse.setStatus(HttpServletResponse.SC_NO_CONTENT);
+        return null;
     }
 
 }
--- src/test/java/com/kshitij/clinicaltrial/controller/InquiryControllerTest.java
+++ src/test/java/com/kshitij/clinicaltrial/controller/InquiryControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +24,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @WebMvcTest(controllers = InquiryController.class)
@@ -70,6 +73,36 @@ class InquiryControllerTest {
         for (int i = 0; i < size; i++) {
             Assertions.assertEquals(expectedPatients.get(i), patients.get(i));
         }
+    }
+
+    @Test
+    @DisplayName("fetch patient by id")
+    public void testGetPatient_givenPatientId_returnPatientInformation() throws Exception {
+        final RequestBuilder request = MockMvcRequestBuilders.get("/patient/4");
+
+        final Patient patient = generator.nextObject(Patient.class);
+
+        when(patientRegistry.getPatient(any(Integer.class))).thenReturn(Optional.of(patient));
+
+        final MvcResult mvcResult = mockMvc.perform(request).andReturn();
+
+        final Patient actualPatient = deserializer.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<>() {});
+
+        Assertions.assertEquals(patient, actualPatient);
+        Assertions.assertEquals(200, mvcResult.getResponse().getStatus());
+    }
+
+    @Test
+    @DisplayName("fetch nothing when patient not found")
+    public void testGetPatient_givenInvalidPatientId_returnNoContent() throws Exception {
+        final RequestBuilder request = MockMvcRequestBuilders.get("/patient/-383");
+
+        when(patientRegistry.getPatient(any(Integer.class))).thenReturn(Optional.empty());
+
+        final MvcResult mvcResult = mockMvc.perform(request).andReturn();
+
+        Assertions.assertEquals("", mvcResult.getResponse().getContentAsString());
+        Assertions.assertEquals(204, mvcResult.getResponse().getStatus());
     }
 
 }
